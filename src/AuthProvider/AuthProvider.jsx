96d517a25525a66d4458b22e2c5fa7dc8f7c9d9f
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2024-10-03 23:46:07 +0600

    on auth state change and show name and photo on navbar

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,24 +1,41 @@
 import PropTypes from "prop-types";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { createContext } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import { auth } from "../Firebase/Firebase.config";
 
 // Create a context
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log("on auth working", currentUser);
+
+        })
+        return () => {
+            unSubscribe();
+        }
+    }, [])
 
     const userCreate = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const userLogin = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
     const authInfo = {
         name: "Md. Mosarof Hossen",
         userCreate,
         userLogin,
+        user,
+        loading,
+
 
     };
 
--- src/Shared/SubNav.jsx
+++ src/Shared/SubNav.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
 
 const SubNav = () => {
+    const { user } = useContext(AuthContext);
+    console.log(user)
     return (
         <div className="navbar bg-[#5D62FF] text-primary-content text-[#dbd7d7]">
             <button className="btn btn-ghost text-xl font-bold"><span className="text-[#dbd7d7]">Travel</span><span className="text-[#FAA61A] -ml-2">Time</span></button>
@@ -11,6 +15,15 @@ const SubNav = () => {
                     <li><Link to="#">Twitter</Link></li>
                     <li><Link to="#">Portfolio</Link></li>
                 </ul>
+            </div>
+            <div className="pr-8 flex gap-4">
+                <h1><span className="font-bold uppercase">Name : <span className="text-[#DAC675]">{user?.displayName
+                }</span> </span> </h1>
+                <div className="avatar">
+                    <div className="ring-primary ring-offset-base-100 w-10 rounded-full ring ring-offset-2">
+                        <img src={user?.photoURL} />
+                    </div>
+                </div>
             </div>
         </div>
 
